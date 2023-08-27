60b52e41e38a1ce017a49ecc1e26b18621f31534
InnaGGB <sh-noki@yandex.ru>
2023-08-27 21:17:54 +0300

    update selectors

--- Unit_1/lesson6_step11.py
+++ Unit_1/lesson6_step11.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 try: 
@@ -7,18 +8,18 @@ try:
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element_by_css_selector('input[placeholder="Input your first name"]')
+    input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_css_selector('input[placeholder="Input your last name"]')
+    input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_css_selector('input[placeholder="Input your email"]')
+    input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
     input3.send_keys("qwerty@mail.com")
         
     # ждем загрузки страницы
     time.sleep(3)
 
     # Отправляем заполненную форму
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
     # Проверяем, что смогли зарегистрироваться
@@ -26,7 +27,7 @@ try:
     time.sleep(1)
 
     # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
 
